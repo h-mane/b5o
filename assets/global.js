a15017f4af63eee6a6bb8ef19153ff65dbec7c46
Mane <maneharutyunian95@gmail.com>
2019-03-15 10:44:13 +0400

    Modal for footer elements

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+    $('#footerModal').on('shown.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var template = button.data('template');
+        var modal = $(this);
+        $.get(template, function (data) {
+            modal.find('.modal-body').html(data);
+        });
+    })
 
+    $('#footerModal').on('hidden.bs.modal', function () {
+        $(this).find('.modal-body').html('');
+    })
 });
